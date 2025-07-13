f2ddfb564e2e693ad3ed7944a265cd3b075bd839
Orzhov <alouis@stagiaire-humanbooster.com>
2025-07-13 20:22:03 +0200

    Add cart.html

--- src/main/java/com/hb/cda/thymeleafproject/repository/CartItemRepository.java
+++ src/main/java/com/hb/cda/thymeleafproject/repository/CartItemRepository.java
@@ -1,6 +1,10 @@
 package com.hb.cda.thymeleafproject.repository;
 
+import com.hb.cda.thymeleafproject.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CartItemRepository extends JpaRepository {
+@Repository
+
+public interface CartItemRepository extends JpaRepository <CartItem, String> {
 }
--- src/main/java/com/hb/cda/thymeleafproject/repository/ProductRepository.java
+++ src/main/java/com/hb/cda/thymeleafproject/repository/ProductRepository.java
@@ -2,6 +2,9 @@ package com.hb.cda.thymeleafproject.repository;
 
 import com.hb.cda.thymeleafproject.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 
 public interface ProductRepository extends JpaRepository <Product, String> {
 }
